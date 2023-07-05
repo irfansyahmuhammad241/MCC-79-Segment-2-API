173b4cd887af6da7b2f95ecc474576815604f358
Muhammad irfansyah <irfansyahmuhammad241@gmail.com>
2023-07-05 16:54:31 +0700

    Added New Project Client

---
+++ Client/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿console.log("Latihan Javascript")
+
+function ChangeColor()
+{
+    var tes1 = document.getElementById("tes1")
+    tes1.style.backgroundColor = "red";
+  
+}
+
+function ChangeText()
+{
+    var tes2 = document.getElementById("tes2")
+    tes2.textContent = "BLUE!!!!";
+    tes2.style.fontSize = "30px";
+    
+
+}
+
+function ChangeColorAndText()
+{
+    var tes4 = document.getElementById("tes4")
+    tes4.style.backgroundColor = "aqua"
+    tes4.textContent = "Dipencet!!!"
+}
